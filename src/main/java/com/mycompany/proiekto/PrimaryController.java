5b867fe42ebf9beb46fef2433a05f40e7ccf1e3f
kaietba <kaietbaraya@gmail.com>
2024-11-11 11:23:38 +0100

    cambios prueba

--- src/main/java/com/mycompany/proiekto/PrimaryController.java
+++ src/main/java/com/mycompany/proiekto/PrimaryController.java
@@ -12,5 +12,6 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+        System.out.println("asdasd");
     }
 }
